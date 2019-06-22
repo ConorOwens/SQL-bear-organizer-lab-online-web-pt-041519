cdba89d81fecb90cb5eedade12e2e37ae5d9675e
Conor Owens <conorj.owens@gmail.com>
2019-06-22 00:47:41 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,5 +4,5 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabith
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 7, "male", "brown", "solitary", "true")
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 1, "female", "brown", "sweet", "true")
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 9, "male", "black", "mean", "true")
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "male", "brown", "friendly", "true")
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 2, "female", "brown", "sassy", "true")
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "male", "brown", "friendly", "true")
