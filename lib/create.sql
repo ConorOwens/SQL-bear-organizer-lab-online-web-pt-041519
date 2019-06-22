ed9d52ca4460f2881089eeb47bef742508aa291d
Conor Owens <conorj.owens@gmail.com>
2019-06-22 00:40:24 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,9 +1,9 @@
-create table bears (
-  id integer primary key,
-  name text,
-  age integer,
-  gender text,
-  color text,
-  temperament text,
-  alive numeric
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  gender TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive INTEGER
 );
